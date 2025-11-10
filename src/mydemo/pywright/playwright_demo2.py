683b905e7554ed7278740cc934533dca030f98ba
jiaxiaopeng <jiaxiaopeng@kuaishou.com>
2025-11-10 20:16:09 +0800

    update:save

--- src/mydemo/pywright/playwright_demo2.py
+++ src/mydemo/pywright/playwright_demo2.py
@@ -6,13 +6,55 @@ from mydemo.utils.download import save_bytes
 import os
 from urllib.parse import urlparse, urljoin
 
+
+### 测试下载
+
 # 设置下载目录（绝对路径）
-download_dir = os.path.abspath("/Users/jiaxiaopeng/Downloads/1")
-# os.makedirs(download_dir, exist_ok=True)
+download_dir = os.path.abspath("/Users/jiaxiaopeng/opt/")
+os.makedirs(download_dir, exist_ok=True)
+
+def handle_route(route):
+    response = route.fetch()
+    content = response.body()
+    print(response.url)
+    suggested = response.headers.get("content-disposition", "")
+    print(suggested)
+    name = infer_file_name(response.url, "mp3")
+    # 保存到文件
+    with open(path := os.path.join(download_dir, name), "wb") as f:
+        f.write(content)
+        print(path)
+    route.continue_()  # 或 abort() 如果不想加载到页面
+
+def download1():
+    cb = ChromeBrowser()
+    page = cb.get_new_page()
+    # 拦截 jpg/png/mp3 请求
+    page.route("**/*.{jpg,jpeg,png,mp3}", handle_route)
+
+    page.goto("https://www.21voa.com/special_english/wilbur-and-orville-wright-the-first-airplane-93397.html")
+    page.wait_for_load_state("networkidle")
+
+    # mp3_src = page.eval_on_selector("xpath=//*[@id='mp3']", "el => el.href")
+    # print(f"mp3_src: {mp3_src}")
+    page.click("xpath=//*[@id='mp3']")
 
 
-cb = ChromeBrowser()
 
+def download2():
+    cb = ChromeBrowser()
+    page = cb.get_new_page()
+    page.goto("https://www.21voa.com/special_english/wilbur-and-orville-wright-the-first-airplane-93397.html")
+    page.wait_for_load_state("networkidle")
+    with page.expect_download() as download_info:
+        page.click("xpath=//*[@id='mp3']")
+
+    download = download_info.value
+    # # # 保存到指定路径
+    save_path = os.path.join(download_dir, download.suggested_filename)
+    download.save_as(save_path)
+
+    print(f"文件已下载并保存到: {save_path}")
 
 # 监听新页面（用于后续操作）
 # new_page = None
@@ -47,13 +89,13 @@ def handle_response(response):
                 print(f"读取响应体失败: {e}")
 
 
-page = cb.get_new_page()
 
-page.on("download", lambda download: handle_download(download, download_dir))
+
+# page.on("download", lambda download: handle_download(download, download_dir))?
 
 # page.on("response", handle_response)
 # page.goto("https://haowallpaper.com/homeViewLook/17854958797835648")
-page.goto("https://www.meilisearch.com/docs/learn/self_hosted/getting_started_with_self_hosted_meilisearch")
+# page.goto("https://www.meilisearch.com/docs/learn/self_hosted/getting_started_with_self_hosted_meilisearch")
 # Playwright 会自动等待元素出现并可交互
 button_xpath = '//*[@id="content"]/span[11]/a'  # 👈 根据实际情况修改 XPath
 # try:
@@ -74,21 +116,21 @@ button_xpath = '//*[@id="content"]/span[11]/a'  # 👈 根据实际情况修改 
 # except TimeoutError:
 #     print("❌ 超时：未找到按钮或按钮不可见")
 
-href = page.get_attribute(button_xpath, "href")
-print(href)
-full_url = urljoin(page.url, href)
-print(full_url)
-cookies = {c["name"]: c["value"] for c in cb._context.cookies()}
-cb._context.storage_state(path="cookies.json")
-resp = requests.get(full_url, cookies=cookies, headers={"Referer": page.url})
-with open("/Users/jiaxiaopeng/Downloads/movies.json" , "wb") as f:
-    f.write(resp.content)
-print("✅ 通过 requests 下载成功")
+# href = page.get_attribute(button_xpath, "href")
+# print(href)
+# full_url = urljoin(page.url, href)
+# print(full_url)
+# cookies = {c["name"]: c["value"] for c in cb._context.cookies()}
+# cb._context.storage_state(path="cookies.json")
+# resp = requests.get(full_url, cookies=cookies, headers={"Referer": page.url})
+# with open("/Users/jiaxiaopeng/Downloads/movies.json" , "wb") as f:
+#     f.write(resp.content)
+# print("✅ 通过 requests 下载成功")
 
 # page.wait_for_timeout(1000)  # 给一点时间让新页面触发
 # page.click(button_xpath)
-with page.expect_download(timeout=10000) as download_info:  # 👈 关键：监听下载
-    page.click(button_xpath)
+# with page.expect_download(timeout=10000) as download_info:  # 👈 关键：监听下载
+#     page.click(button_xpath)
 
 # if new_page is None:
 #         print("未检测到新页面打开！")
@@ -111,16 +153,16 @@ with page.expect_download(timeout=10000) as download_info:  # 👈 关键：监�
 
 
 # 获取下载对象
-download = download_info.value
-# # 保存到指定路径
-save_path = os.path.join(download_dir, download.suggested_filename)
-print(f"save_path: {save_path}")
-download.save_as(save_path)
 #
-# print(f"文件已下载并保存到: {save_path}")
+
 
 # if new_page is not None:
 #     # 关闭新页面
 #     new_page.close()
 # 关闭当前页
-page.close()
+# page.close()
+
+
+if __name__ == "__main__":
+    # handle_download(download=download, path=save_path)
+    download1()
